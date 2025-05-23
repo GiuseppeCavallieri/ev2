90350a17bdac59a81de3714572e57463e418bf50
GiuseppeCavallieri <giuseppe.cavallieri@usach.cl>
2025-05-23 13:12:18 -0400

    conexion a eureka msuser

--- backend/ms-user/src/main/java/com/example/ms_user/MsUserApplication.java
+++ backend/ms-user/src/main/java/com/example/ms_user/MsUserApplication.java
@@ -1,4 +1,4 @@
-package com.tutorial.ms_user;
+package com.example.ms_user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
