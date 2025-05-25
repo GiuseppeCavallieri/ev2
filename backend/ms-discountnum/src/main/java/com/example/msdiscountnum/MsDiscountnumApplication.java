7c3aa7aacd3c06204512930dfe3900477d1945c9
GiuseppeCavallieri <giuseppe.cavallieri@usach.cl>
2025-05-25 00:28:44 -0400

    ms-discountnum

---
+++ backend/ms-discountnum/src/main/java/com/example/msdiscountnum/MsDiscountnumApplication.java
@@ -0,0 +1,13 @@
+package com.example.msdiscountnum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDiscountnumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsDiscountnumApplication.class, args);
+    }
+
+}
---
+++ backend/ms-discountnum/src/test/java/com/example/msdiscountnum/MsDiscountnumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msdiscountnum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDiscountnumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
